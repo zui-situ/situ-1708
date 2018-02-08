29c8a7d8bc7a73f306dc55608fc16c935e55fe13
situ <382808911@qq.com>
2018-02-08 11:48:01 +0800

    提交购物车“和详情页静态页面

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,8 @@
 require.config({
     paths:{
         'jquery':'../lib/jquery-3.2.1',
-        'boot':'../lib/bootstrap/js/bootstrap',
     },
-    shim:{
-        'boot':['jquery'],
+    slim:{
     }
 
 })
